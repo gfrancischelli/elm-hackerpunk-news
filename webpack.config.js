b26487292716815c09ce2beb4a69685f319adbc7
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-05-04 01:15:34 -0300

    :wrench: Add Babel es6 support

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+            options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
         test: /\.(css|scss)$/,
         use: [
           'style-loader',
