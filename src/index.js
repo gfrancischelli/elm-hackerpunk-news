2f74c4b4295d5c3b22d518a622f17c5a6a336571
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-05-04 21:34:31 -0300

    Format index.js with prettier

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
-'use strict'
+"use strict";
 
 import './index.html'
 
-import api from './database'
-import Elm from './Main.elm'
+import api from "./database";
+import Elm from "./Main.elm";
 
-const mountNode = document.getElementById('main')
-const app = Elm.Main.embed(mountNode)
+const mountNode = document.getElementById("main");
+const app = Elm.Main.embed(mountNode);
 
-
-const feedsPort = app.ports.updateFeedIds
-const updateItem = app.ports.updateItem
+const feedsPort = app.ports.updateFeedIds;
+const updateItem = app.ports.updateItem;
 
 app.ports.subscribeToFeed.subscribe(feed => {
-  api.child(feed).off()
-  api.child(feed).on('value', snapshot => feedsPort.send(snapshot.val()))
+  api.child(feed).off();
+  api.child(feed).on("value", snapshot => feedsPort.send(snapshot.val()));
 });
-
 
 app.ports.getStoriesById.subscribe(ids => {
   ids.forEach(id => {
-    api.child(`/item/${id}`).off()
-    api.child(`/item/${id}`).on('value', snapshot => {
-      const item = snapshot.val()
-      item["type_"] = item.type
-      updateItem.send(item)
-    })
-  })
- })
+    api.child(`/item/${id}`).off();
+    api.child(`/item/${id}`).on("value", snapshot => {
+      const item = snapshot.val();
+      item["type_"] = item.type;
+      updateItem.send(item);
+    });
+  });
+});
